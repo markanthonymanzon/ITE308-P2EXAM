5e47c69a23cdd45126d55c29652f22dd26cbc184
Anthony Manzon <114978074+markanthonymanzon@users.noreply.github.com>
2022-10-15 01:53:21 +0800

    Add files via upload

---
+++ ITE308-P2EXAM/ordersummary.php
@@ -0,0 +1,23 @@
+
+  <html>
+ <link rel="stylesheet" href="style.css">
+ <header id = "head">Your Summary Order: </header>
+    <body class = "form1">
+      Customer Name: <?php echo $_POST["name"]; ?> <br>
+      Age : <?php echo $_POST["age"]; ?> <br>
+      Gender : <?php echo $_POST["gender"]; ?> <br>
+      Address : <?php echo $_POST["address"]; ?> <br>
+      Movie : <?php echo $_POST["movie"]; ?> <br>
+      Ticket : <?php echo $_POST["ticket"]; ?> <br>
+      Adds-ons: <?php echo $_POST["add1"]; ?> <br>
+      Adds-ons: <?php echo $_POST["add2"]; ?> <br>
+      Adds-ons: <?php echo $_POST["add3"]; ?> <br>
+
+
+   </body>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/1TO48Cnl66w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<footer id = "footer"> Thank You!!! and come again:)</footer>
+</html> 
+
+
+
